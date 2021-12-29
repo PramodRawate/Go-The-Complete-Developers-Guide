3463f0d2f5383224924de6cd1e62fabbb5824867
RawatePramod <rawatepramosmd@gmail.com>
2021-12-29 16:39:05 +0530

    Converting deck type to string(comma seperated)

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Create a new type of 'deck'
 // which is a slice of strings
@@ -29,4 +32,8 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:3], d[3:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
 }
--- cards/main.go
+++ cards/main.go
@@ -1,13 +1,8 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDec()
-	cards.print()
-
-	hand, remainingCards := deal(cards, 3)
-	println("********Hand cards")
-	hand.print()
-	println("********Remaining cards")
-	remainingCards.print()
-
+	fmt.Println(cards.toString())
 }
